97db0a829f9b69b7d9c13ae349ac6556effaff4e
ikechukwu kevin-chika <ikechukwukevin725@gmail.com>
2024-01-14 06:41:13 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+ /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 10,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.navlist a[href*=' + sectionId + ']')
+
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive);
+
+    
